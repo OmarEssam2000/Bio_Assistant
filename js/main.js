73aa1fa5871164c5b7f6debe26fb6569bc3599cf
OmarEssam2000 <74010778+OmarEssam2000@users.noreply.github.com>
2022-01-03 08:49:06 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// var user = document.getElementById("user");
+// user.addEventListener("focus",()=>{
+//     user.style.color="#45748C"
+// })
+var x = document.querySelectorAll(".tr")
+x.forEach(c =>{
+    c.addEventListener("focus",()=>{
+        c.style.color="#45748C"
+    })
+})
